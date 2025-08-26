afd278688a26c36b15ff3f459844860f26768588
Alex Santiago <davisitosantiago11@gmail.com>
2025-08-25 21:01:30 -0400

    ReadTheDocs Version Change

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -10,7 +10,7 @@
 project = 'ARES'
 copyright = '2025, Alex D. Santiago-Vargas, José Abraham Bolaños Vargas'
 author = 'Alex D. Santiago-Vargas, José Abraham Bolaños Vargas'
-release = '1.0'
+release = '1.1'
 ARES_logo = './../../../src/assets/ARES Icon.png'
 ARES_favicon = './../../../src/assets/ARES Icon.png'
 
